a7627c97cbdfdc64c2d9cd6360abe52897a5edb9
Pauline Barnades <pauline.barnades@stenaline.com>
2023-03-10 14:08:36 +0100

    adding one user case

--- 7-Creating_Custom_Transactions/src/LoginPage.js
+++ 7-Creating_Custom_Transactions/src/LoginPage.js
@@ -1,48 +1,57 @@
 import { useState } from "react";
 import { AuthProvider } from "oreid-js";
 import { useOreId } from "oreid-react";
-
+import "./App.css";
 
 export const LoginPage = () => {
-	const oreId = useOreId();
-	const [error, setError] = useState("");
+  const oreId = useOreId();
+  const [error, setError] = useState("");
 
-	const onError = (error) => {
-		console.log("Login failed", error);
-		setError(error);
-	};
+  const onError = (error) => {
+    console.log("Login failed", error);
+    setError(error);
+  };
 
-	const onSuccess = ({ user }) => {
-		console.log("Login successfull. User Data: ", user);
-	};
-    
-	const loginWithProvider = (provider) => {
-		oreId.popup
-			.auth({
-				provider,
-			})
-			.then( onSuccess )
-			.catch( onError );
-	};
+  const onSuccess = ({ user }) => {
+    console.log("Login successfull. User Data: ", user);
+  };
 
-	return (
-        <div>
-			<button
-				onClick={() => {
-					loginWithProvider(AuthProvider.Google);
-				}}
-			>
-				Google
-			</button>
-			<button
-				onClick={() => {
-					loginWithProvider(AuthProvider.Email);
-				}} 
-            >
-				Email
-			</button>
+  const loginWithProvider = (provider) => {
+    oreId.popup
+      .auth({
+        provider,
+      })
+      .then(onSuccess)
+      .catch(onError);
+  };
 
-			{error && <div>Error: {error.message}</div>}
+  return (
+    <div>
+      <div className="box">
+        <h1 className="title">Congrats!🥳 </h1>
+        <h2 classname="h2">
+          You have adquired 2 POAPs.Would you like us to keep them safe in a
+          web3 wallet?
+        </h2>
+        <div classname="buttons">
+          <button
+            onClick={() => {
+              loginWithProvider(AuthProvider.Google);
+            }}
+          >
+            Log in with Google
+          </button>
+          <button
+            onClick={() => {
+              loginWithProvider(AuthProvider.Email);
+            }}
+          >
+            Log in with Email
+          </button>
+
+          {error && <div>Error: {error.message}</div>}
         </div>
-	);
+      </div>
+    </div>
+  );
 };
